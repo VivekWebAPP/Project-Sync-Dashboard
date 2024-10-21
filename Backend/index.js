6f21a6b0466567b7ec41d7cd0217f9d8b3ba7f87
Vivek Shankar Phadake <129990463+Vivek20sp@users.noreply.github.com>
2024-10-21 22:26:02 +0530

    Third Commit

--- Backend/index.js
+++ Backend/index.js
@@ -11,10 +11,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
+const assiginedOrigin = [
+    'http://localhost:3000',
+]
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
-
-app.use(cors());
+app.use(cors({
+    origin: assiginedOrigin, // Allow requests from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+    credentials: true, // If your requests include credentials like cookies
+}));
 
 connectWithDatabase();
 
@@ -28,7 +35,6 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port: http://localhost:${port}`);
 });
